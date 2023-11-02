28d34dc04bb59153a9d5214d20c4692f0a60004f
gulzat.d <ms.gdlr223@gmail.com>
2023-11-02 20:39:37 +0500

    finally

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
+import { FfmpegExecutor } from "./commands/ffmpeg/ffmpeg.executor";
+import { ConsoleLogger } from "./out/console-logger/console-logger";
+
 export class App {
-  run() {
-    console.log("Done");
+  async run() {
+    new FfmpegExecutor(ConsoleLogger.getInstance()).execute();
   }
 }
 
